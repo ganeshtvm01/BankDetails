5acc50d93768c749b3fad000ff3d22f434de4f2b
ganeshtvm01 <See>
2022-08-29 11:17:41 +0530

    new program

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,9 @@
+package org.bank;
+
+public class AxisBank {
+	public void deposit() {
+		int a=132422;
+		System.out.println("Axis Bank Deposit is "+a);
+	}
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,23 @@
+package org.bank;
+
+public class BankInfo extends AxisBank{
+	private void saving() {
+		float a=22.7657f;
+		System.out.println("savings is "+a);
+	}
+	private void fixed() {
+		float a=46675.7657f;
+		System.out.println("fixed is "+a);
+
+
+	}
+	public static void main(String[] args) {
+		BankInfo a = new BankInfo();
+		a.deposit();
+		a.saving();
+		a.fixed();
+		li
+		
+	}
+
+}
